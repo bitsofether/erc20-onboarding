4267afafd0adea9761d9b51bfe486d2b3c4bca5f
Benjamin Scholtz <contact@benjaminscholtz.com>
2020-05-10 17:59:30 +0200

    Add deployment procedure

--- shell.nix
+++ shell.nix
@@ -8,6 +8,39 @@ stdenv.mkDerivation {
     git
   ];
   shellHook = ''
-   
+    #ERC20_MIGRATOR_ADDRESS=
+    #LEGACY_TOKEN_ADDRESS=
+    truffle-console() {
+      npx truffle console --network local
+      #truffle(local)> compile
+      #truffle(local)> owner = (await web3.eth.getAccounts())[1]
+      #truffle(local)> legacyToken = await MyLegacyToken.new({ from: owner })
+      #truffle(local)> legacyToken.address
+      #truffle(local)> (await legacyToken.balanceOf(owner)).toString()
+    }
+    oz-init_1() {
+      npx oz init token-migration 1.0.0
+    }
+    oz-configure_2() {
+      npx oz link @openzeppelin/contracts-ethereum-package
+      npx oz add MyUpgradeableToken
+    }
+    oz-deploy_3() {
+      echo 'Option --deploy-dependencies only necessary for dev'
+      npx oz push -n local --deploy-dependencies
+    }
+    oz-deploy-migrator_4() {
+      set -u
+      npx oz add ERC20Migrator
+      npx oz push -n local --deploy-dependencies
+      npx oz deploy -n local ERC20Migrator $LEGACY_TOKEN_ADDRESS
+      set +u
+    }
+    oz-deploy-token_5() {
+      ADDRESS=$(npx oz deploy -n local -k upgradeable MyUpgradeableToken)
+      #$LEGACY_TOKEN_ADDRESS,$ERC20_MIGRATOR_ADDRESS
+      npx oz call -n local --to $ADDRESS
+dbD9F43cCd776D7664C --method initialize --args $LEGACY_TOKEN_ADDRESS,$ERC20_MIGRATOR_ADDRESS
+    }
   '';
 }
