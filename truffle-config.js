28d2eda5811a7887f8adc29bd0b11227793ad877
Benjamin Scholtz <contact@benjaminscholtz.com>
2020-05-10 17:04:36 +0200

    Add deployment config

--- truffle-config.js
+++ truffle-config.js
@@ -1,5 +1,26 @@
 'use strict';
 
+require('dotenv').config();
+
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const mnemonic = process.env.MNEMONIC;
+const network_id = process.env.NETWORK_ID;
+
+var network;
+
+switch (network_id) {
+  case '0':
+   network = 'mainnet';
+   break;
+  case '4':
+   network = 'rinkeby';
+   break;
+  default:
+   network = 'rinkeby';
+}
+
+const rpc_url = `https://${network}.infura.io/v3/${process.env.INFURA_PROJECT_ID}`;
+
 module.exports = {
   networks: {
     local: {
@@ -8,6 +29,12 @@ module.exports = {
       gas: 5000000,
       gasPrice: 5e9,
       network_id: '*'
+    },
+    remote: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, rpc_url)
+      },
+      network_id: network_id
     }
   }
 };
